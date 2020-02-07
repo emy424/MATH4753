aa9baeeecabbcb4fdb2ceddca320db3e08849894
Emily Lu <Emily.lu@ou.edu>
2020-02-07 15:21:48 -0600

    initial files

---
+++ R/myf.R
@@ -0,0 +1,13 @@
+#' Square function
+#'
+#'Takes a vector and returns a vector of squared components
+#' @param x
+#'
+#' @return a vector of squared components in the form of vector
+#' @export
+#'
+#' @examples
+#' x=1:30; myf(x)
+myf=function(x){
+  x^2
+}
