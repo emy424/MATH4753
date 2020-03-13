9b2369073ef399781c5e535218e2d0a0d8dda75e
Emily Lu <Emily.lu@ou.edu>
2020-03-13 12:30:54 -0500

    data and function update

---
+++ R/fire.R
@@ -0,0 +1,12 @@
+#' Fire data set.
+#'
+#' A data set collected by
+#'
+#'@format A data fram with 15 rows and 2 variables
+#' \describe{
+#'    \item{DISTANCE}{distance from fire station in miles}
+#'    \item{DAMAGE}{x 1000 dollars in damage}
+#'}
+#'
+#'@source \url{https://www.crcpress.com/Statistics-for-Engineering-and-the-Sciences/Mendenhall-Sincich/p/book/9781498728850}
+"fire"
